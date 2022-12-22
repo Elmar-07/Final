5a376f25af89db9623e961db82770037b6bb3e99
Elmar-07 <nurmamedov572@gmail.com>
2022-12-22 22:35:14 +0400

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,8 +12,8 @@
 
 
   let menuBtn = document.querySelector('.menu-btn');
-let menu = document.querySelector('.menu');
-menuBtn.addEventListener('click', function(){
+  let menu = document.querySelector('.menu');
+  menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 })
