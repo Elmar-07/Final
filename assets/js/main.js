00e6d940ea2d64dad278aeec7cb0078179b91637
Elmar-07 <nurmamedov572@gmail.com>
2022-12-22 22:22:45 +0400

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,12 +11,12 @@
   });
 
 
-//   let menuBtn = document.querySelector('.menu-btn');
-// let menu = document.querySelector('.menu');
-// menuBtn.addEventListener('click', function(){
-// 	menuBtn.classList.toggle('active');
-// 	menu.classList.toggle('active');
-// })
+  let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
 
 
   
