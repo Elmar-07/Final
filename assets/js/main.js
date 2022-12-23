553d6c073c1ba8585cf9543fad56466188ed41a8
Elmar-07 <nurmamedov572@gmail.com>
2022-12-23 19:15:28 +0400

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
 
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
-    spaceBetween: -50,
+    spaceBetween: 30,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
