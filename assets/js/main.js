1a49bf69adcde898f798c3e9712196ab5a9fdd40
Elmar-07 <nurmamedov572@gmail.com>
2022-12-24 16:05:20 +0400

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,44 @@
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
+    grabCursor:"true",
+
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
+      dynamicBullets: "true"
     },
-  });
+
+    breakpoints: {
+      0: {
+          slidesPerView: 1,
+          spaceBetween: 30,
+      },
+      520: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+      },
+
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+    },
+
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+  },
+
+      1024: {
+          slidesPerView: 3,
+          spaceBetween: 0,
+      },
+      
+
+    },
+  
+   });
+   
 
 
   let menuBtn = document.querySelector('.menu-btn');
