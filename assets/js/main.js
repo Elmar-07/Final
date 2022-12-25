0e29038962a1e2b8c65d5aebff9bbeafcc5c3912
Elmar-07 <nurmamedov572@gmail.com>
2022-12-25 15:34:23 +0400

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,30 +52,62 @@
 })
 
 
-  
+
+function kura_tm_preloader(){
+	
+	"use strict";
+	
+	var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
+	var preloader = $('#preloader');
+	
+	if (!isMobile) {
+		setTimeout(function() {
+			preloader.addClass('preloaded');
+		}, 800);
+		setTimeout(function() {
+			preloader.remove();
+		}, 2000);
+
+	} else {
+		preloader.remove();
+	}
+}
 
 
-$(function() {
-    $('#navbar.navbar-right ul li a').click(function() {
-      //clear active status of any parent LI's
-      $('#navbar.navbar-right ul li').removeClass('active');
+function kura_tm_my_load(){
+	
+	"use strict";
+	
+	var speed	= 500;
+	setTimeout(function(){kura_tm_preloader();},speed);
+	setTimeout(function(){jQuery('body').addClass('opened');},speed+2000);
+}
 
-      // store id of new active sub-nav
-      var currSub = $(this).parent();
-      currSub.addClass('active')
-      var id = currSub.attr('id');
 
-      // clear active status of any sub-nav list
-      $('#subnavbar ul.navbar-nav').removeClass('active');
 
-      // set selected sub-nav to active
-      $('.' + id).addClass('active');
+// $(function() {
+//     $('#navbar.navbar-right ul li a').click(function() {
+//       //clear active status of any parent LI's
+//       $('#navbar.navbar-right ul li').removeClass('active');
 
-      console.log($('.' + id).attr('class'));
+//       // store id of new active sub-nav
+//       var currSub = $(this).parent();
+//       currSub.addClass('active')
+//       var id = currSub.attr('id');
 
-    });
+//       // clear active status of any sub-nav list
+//       $('#subnavbar ul.navbar-nav').removeClass('active');
 
-  });
+//       // set selected sub-nav to active
+//       $('.' + id).addClass('active');
+
+//       console.log($('.' + id).attr('class'));
+
+//     });
+
+//   });
+
+
 
 
 
