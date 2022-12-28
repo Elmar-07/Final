80eaef1a7e11a898f658007842ea4fd17dad60ba
Elmar-07 <nurmamedov572@gmail.com>
2022-12-28 19:36:00 +0400

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,10 +53,7 @@
     },
   
 
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  
 
    });
    
@@ -125,5 +122,16 @@ $(document).on("scroll", function() {
 
 
 
+
+$(document).ready(function() {
+  setTimeout(function() {
+    $('#container').addClass('loaded');
+    // Once the container has finished, the scroll appears
+    if ($('#container').hasClass('loaded')) {
+      // It is so that once the container is gone, the entire preloader section is deleted
+      $('#preloader').delay(9000).queue(function() {
+        $(this).remove();
+      });}
+  }, 3000);});
 
 
